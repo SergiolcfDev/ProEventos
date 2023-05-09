deb008e7e938c0cda992c06899174bdb628e291e
Sergio Ferreira <sergiolcf.dev@hotmail.com>
2023-05-08 23:17:28 -0300

    Seção2: Dotnet 5

---
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+        public EventosController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+        }
+    }
+}
