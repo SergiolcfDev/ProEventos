9a8bf9cd3d443b89c37aa562625426cc4c997be0
Sergio Ferreira <sergiolcf.dev@hotmail.com>
2023-05-09 00:05:59 -0300

    Seção3: EF Core 5

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,14 +13,21 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventosController : ControllerBase
     {
-        public EventosController()
+        private readonly DataContext _context;
+        public EventosController(DataContext context)
         {
-
+            this._context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
+            return _context.eventos;
+        }
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return _context.eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
     }
 }
