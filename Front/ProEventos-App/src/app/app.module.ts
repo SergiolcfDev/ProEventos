99db5a1612c1b6a23fb9a95b8500e133a9e922d7
Sergio Ferreira <sergiolcf.dev@hotmail.com>
2023-05-09 22:56:49 -0300

    Ajustes nas versões do Node, Angular CLI, Angular e Dependencias e Inicio da Seção 5

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import{ HttpClientModule }from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './Palestrantes/Palestrantes.component';
+import { NavComponent } from './nav/nav.component';
+import {CollapseModule } from 'ngx-bootstrap/collapse'
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-      PalestrantesComponent
+      PalestrantesComponent,
+      NavComponent
    ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
+      CollapseModule.forRoot(),
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
