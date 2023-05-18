a1edcfbc3610c175a857b4bd9a8ac3ab918f5b5b
Sergio Ferreira <sergiolcf.dev@hotmail.com>
2023-05-17 23:00:14 -0300

    Seção 5: Angular - Interpolação, Diretivas e Binding

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import{ HttpClientModule }from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
@@ -20,7 +22,8 @@ import { CollapseModule } from 'ngx-bootstrap/collapse'
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
-      CollapseModule.forRoot(),
+    CollapseModule.forRoot(),
+      FormsModule,
     AppRoutingModule,
     HttpClientModule
   ],
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
   public Eventos: any = [];
+  public EventosFiltrados: any = [];
+
   widthImg = 50;
   marginImg = 2;
   showImg = true;
+  private _filtrarLista: string = '';
+
+  public get filtroLista() {
+    return this._filtrarLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtrarLista = value;
+    this.EventosFiltrados = this.filtroLista ? this.filterEvents(this.filtroLista) : this.Eventos;
+  }
+
+  filterEvents(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.Eventos.filter((ev: any) => ev.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+                                            ev.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1)
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -24,7 +42,10 @@ export class EventosComponent implements OnInit {
   public GetEventos(): void{
 
     this.http.get('https://localhost:5001/api/Eventos').subscribe(
-      response => this.Eventos = response,
+      response => {
+        this.Eventos = response;
+        this.EventosFiltrados = this.Eventos;
+      },
       error => console.log(error)
     );
 }
